85d0d27ba102b7388d7de3978c24e27dc08c6aa2
felixbyrjall <felixbyrjall@gmail.com>
2024-05-25 15:46:27 +0200

    test for heroku dev page

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Lightbeam by Cygnus</h1>);
+root.render(<h1>Dev page for Lightbeam by Cygnus</h1>);
