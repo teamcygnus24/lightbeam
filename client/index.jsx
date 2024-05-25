46389cd12488ceaef56eea8a0e52ed20dc6cce99
Henry M <Henry@nextdata.no>
2024-05-25 15:33:27 +0200

    testing

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Lightbeam</h1>);
+root.render(<h1>Lightbeam by Cygnus</h1>);
