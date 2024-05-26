adb5ccd9d11e36c466edf561da94a3787224096d
Jokubas Gedminas <jokas3@gmail.com>
2024-05-26 13:51:53 +0200

    Connected Server to MongoDB with Mongoose

---
+++ server/routes/loginRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+
+} from "../controllers/loginController.js";
+
+const router = express.Router();
+
+//Login POST
+
+//Login GET
+
+//Login DELETE
+
+//Login UPDATE
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,35 @@
 import express from "express";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import loginRoutes from "./routes/loginRoutes.js";
 
 const app = express();
 app.use(express.static("../client/dist")) // Use static files under client/dist
-app.listen(process.env.PORT || 3000);
+
+//Connect to the database
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to the database")
+    })
+.catch((error) => {
+        console.log(error)
+    });
+
+//Request logger
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+//Request Listener
+app.listen(process.env.PORT, () => {
+
+    console.log(`Listening to port ${process.env.PORT}`);
+});
+
+//Routes
+app.use("/api/login", loginRoutes);
