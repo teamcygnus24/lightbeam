6adbc99bf6d26d5d2ed49d29f7ec6cddd37c5007
Felix Byrjall <felixbyrjall@gmail.com>
2024-05-26 16:50:40 +0200

    added router for display

--- client/components/utility/router.js
+++ client/components/utility/router.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Login } from "../pages/login";
 import { Homepage } from '../pages/homepage';
 import { Projects } from '../pages/projectpage';
-import { Dashboard } from '../pages/dashboardpage'; // Import the Dashboard component
+import { Dashboard } from '../pages/dashboardpage';
+import { Display } from '../pages/displaypage';
 import '../../resources/styles/styles.css';
 
 export function AppRouter() {
@@ -14,6 +15,7 @@ export function AppRouter() {
                 <Route path="/" element={<Homepage />} />
                 <Route path="/projects" element={<Projects />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/display" element={<Display/> } />
             </Routes>
         </Router>
     );
