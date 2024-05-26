4dc0250d9e290a307cdc1fce1319b73921e3a235
Felix Byrjall <felixbyrjall@gmail.com>
2024-05-26 20:37:20 +0200

    update server to handle client-side routing

--- server/server.js
+++ server/server.js
@@ -2,15 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import cookie from "cookie";
-
-import dotenv from "dotenv";
-dotenv.config();
-
 import loginRoutes from "./routes/loginRoutes.js";
 import * as path from "path";
+import dotenv from "dotenv";
 
+//Load environment variables
+dotenv.config();
+
+//Create express app
 const app = express();
-app.use(express.static("../client/dist")) // Use static files under client/dist
+
+// Use static files under client/dist
+app.use(express.static(path.join(__dirname, '../client/dist'))); 
 
 //Middleware
 app.use(express.json())
@@ -26,17 +29,21 @@ app.use((req, res, next) => {
 //Routes
 app.use("/api/login", loginRoutes);
 
+// Catch-all handler to serve the React app for any other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+
 //Connect to the database
 mongoose
-.connect(process.env.MONGO_URI)
-.catch((error) => {
-        console.log(error)
+    .connect(process.env.MONGO_URI)
+    .catch((error) => {
+            console.log(error)
+        })
+    .then(() => {
+        console.log("Connected to the database")
     })
-.then(() => {
-    console.log("Connected to the database")
 
-
-})
 //Request Listener
 app.listen(process.env.PORT, () => {
 
