58dd7b473a9b438aa5b4cf81f3c1020ea6fe8d0b
Henry M <Henry@nextdata.no>
2024-05-30 09:43:43 +0200

    added p in divs in display page

--- client/components/pages/displaypage.jsx
+++ client/components/pages/displaypage.jsx
@@ -32,16 +32,16 @@ export function Display({ displayChange }) {
     return (
         <div className="preview">
             <div className="content">
-                <p>Text 1:</p>{slide.text_01}
-                <p>Text 2:</p> {slide.text_02}
-                <p>Text 3:</p> {slide.text_03}
-                <p>Text 4:</p> {slide.text_04}
-                <p>Text 5:</p> {slide.text_05}
-                <p>Text 6:</p> {slide.text_06}
-                <p>Text 7:</p> {slide.text_07}
-                <p>Text 8:</p> {slide.text_08}
-                <p>Text 9:</p> {slide.text_09}
-                <p>Text 10:</p> {slide.text_10}
+                <div><p>Text 1:</p>{slide.text_01}</div>
+                <div><p>Text 2:</p> {slide.text_02}</div>
+                <div><p>Text 3:</p> {slide.text_03}</div>
+                <div><p>Text 4:</p> {slide.text_04}</div>
+                <div><p>Text 5:</p> {slide.text_05}</div>
+                <div><p>Text 6:</p> {slide.text_06}</div>
+                <div><p>Text 7:</p> {slide.text_07}</div>
+                <div><p>Text 8:</p> {slide.text_08}</div>
+                <div><p>Text 9:</p> {slide.text_09}</div>
+                <div><p>Text 10:</p> {slide.text_10}</div>
             </div>
         </div>
     );
