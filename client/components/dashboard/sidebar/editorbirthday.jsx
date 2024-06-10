b9378718e1ca1b267a61772097ae0e8aab2d4af5
Jokubas Gedminas <jokas3@gmail.com>
2024-06-11 00:31:41 +0200

    heroku variable changes

--- client/components/dashboard/sidebar/editorbirthday.jsx
+++ client/components/dashboard/sidebar/editorbirthday.jsx
@@ -52,7 +52,7 @@ const EditorBirthday = () => {
     }
 
     useEffect(() => {
-        const ws = new WebSocket("ws://localhost:3000/");
+        const ws = new WebSocket("wss://lightbeam-smidig-dev-393006ce2df9.herokuapp.com/");
         ws.onmessage = (event) => {
             console.log(event.data)
         }
