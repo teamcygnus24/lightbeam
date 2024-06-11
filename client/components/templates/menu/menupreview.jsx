40071b8d0c020fcf298eaa97e665c46ef1b0a525
Henry M <Henry@nextdata.no>
2024-06-11 20:46:16 +0200

    corrected editor style for lunch template in preview

--- client/components/templates/menu/menupreview.jsx
+++ client/components/templates/menu/menupreview.jsx
@@ -1,25 +1,26 @@
 import long_food from '../../../resources/images/food.png'
+import '../../styles/templates/menupreview.css';
 
 const MenuPreview = ({ slideInfo }) => {
 
     return (
         <div className="preview-slide-container">
-            <div className='menu-box'>
-                <div className="content">
+            <div className='menu-preview-box'>
+                <div className="preview-content">
                     <h2>Today's menu</h2>
                     <div className="spacer"></div>
-                    <span className="input"><h3>{slideInfo.text_01 ? slideInfo.text_01 : "Title 1"}</h3></span>
+                    <span className="preview-input"><h3>{slideInfo.text_01 ? slideInfo.text_01 : "Title 1"}</h3></span>
                     <div className="line"></div>
-                    <span className="input">{slideInfo.text_02 ? slideInfo.text_02 : "Alternative 1"}</span>
-                    <span className="input">{slideInfo.text_03 ? slideInfo.text_03 : "Alternative 2"}</span>
-                    <span className="input">{slideInfo.text_04 ? slideInfo.text_04 : "Alternative 3"}</span>
-                    <span className="input"><h3>{slideInfo.text_07 ? slideInfo.text_07 : "Title 2"}</h3></span>
+                    <span className="preview-input">{slideInfo.text_02 ? slideInfo.text_02 : "Alternative 1"}</span>
+                    <span className="preview-input">{slideInfo.text_03 ? slideInfo.text_03 : "Alternative 2"}</span>
+                    <span className="preview-input">{slideInfo.text_04 ? slideInfo.text_04 : "Alternative 3"}</span>
+                    <span className="preview-input"><h3>{slideInfo.text_07 ? slideInfo.text_07 : "Title 2"}</h3></span>
                     <div className="line"></div>
-                    <span className="input">{slideInfo.text_08 ? slideInfo.text_08 : "Alternative 1"}</span>
-                    <span className="input">{slideInfo.text_09 ? slideInfo.text_09 : "Alternative 2"}</span>
+                    <span className="preview-input">{slideInfo.text_08 ? slideInfo.text_08 : "Alternative 1"}</span>
+                    <span className="preview-input">{slideInfo.text_09 ? slideInfo.text_09 : "Alternative 2"}</span>
                 </div>
             </div>
-            <div className="image-box">
+            <div className="image-preview-box">
                 <img src={long_food} alt="food pic"/>
             </div>
         </div>
