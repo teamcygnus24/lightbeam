050f1df6d5372890d21e11aa2b5619fb992f436a
Felix Byrjall <felixbyrjall@gmail.com>
2024-06-12 14:34:36 +0200

    Add conditional rendering for image display based on source

--- client/components/templates/birthday/birthday.jsx
+++ client/components/templates/birthday/birthday.jsx
@@ -1,13 +1,28 @@
+import React, { useState } from 'react';
 import '../../styles/templates/birthday.css'
 
 const Birthday = ({ currentSlide }) => {
+
+    const [imgError1, setImgError1] = useState(false);
+    const [imgError2, setImgError2] = useState(false);
+
+    const imgSrc1 = currentSlide?.text_02;
+    const imgSrc2 = currentSlide?.text_03;
+
 
     return (
         <div className="display-birthday-container">
             <div className="display-birthday-box">
                 <h1 className="display-birthday-title">{currentSlide?.text_01}</h1>
-                <img alt="image/video" src={currentSlide?.text_02} className="display-birthday-imagePerson"/>
-                <img alt="image/video" src={currentSlide?.text_03} className="display-birthday-imageAnnouncement"/>
+                <img alt="image/video"
+                    src={imgSrc1}
+                    className="display-birthday-imagePerson"
+                    style={{ display: imgSrc1 && !imgError1 ? 'block' : 'none' }}
+                    onError={() => setImgError1(true)}/>
+                <img src={imgSrc2}
+                    className="display-birthday-imagePerson"
+                    style={{ display: imgSrc2 && !imgError2 ? 'block' : 'none' }}
+                    onError={() => setImgError2(true)}/>
                 <h3 className="display-birthday-desc">{currentSlide?.text_04}</h3>
             </div>
         </div>
